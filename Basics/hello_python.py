fe78835e925f225da1faf957b57cf4baef2d6c6c
SSrivas@7 <s.k.srivastava345@gmail.com>
2025-09-01 01:33:55 +0530

    All Python folders first day

---
+++ Basics/hello_python.py
@@ -0,0 +1,14 @@
+# print("hello Sir")
+
+# def chai(n):
+#     print(n)
+
+# chai("Want some tea")
+#Muutable and Immutable-> Object by reference example.
+# x=10;
+# y=x;
+# print("old x",x)  
+# print("old y",y)
+# x= 15
+# print("new x",x)
+# print ("new y after changing x",y) Value is still pointing 10 for y but changed for x. reference
