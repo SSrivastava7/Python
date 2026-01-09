9176af7e6c7a5d52e8cee7c5a302ce953ebda87f
SSrivas@7 <s.k.srivastava345@gmail.com>
2026-01-09 10:10:50 +0530

    commiting iterator

---
+++ Basics01/itertool.py
@@ -0,0 +1,8 @@
+myList=['1','2','3','4']
+I=iter(myList)
+print(I)
+print(I.__next__())
+print(I.__next__())
+print(I.__next__())
+print(I.__next__())
+print(I.__next__()) # in <module> print(I.__next__()) StopIteration
